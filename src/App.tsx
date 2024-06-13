36e11a29dd6be56cdfb20f41c5f2a806c40c1e21
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-06-13 18:45:38 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,77 @@
-import { ButtonMobile } from '@alfalab/core-components/button/mobile';
-import { CDNIcon } from '@alfalab/core-components/cdn-icon';
-import { Collapse } from '@alfalab/core-components/collapse';
-import { Gap } from '@alfalab/core-components/gap';
-import { SliderInput, SliderInputProps } from '@alfalab/core-components/slider-input';
-import { Typography } from '@alfalab/core-components/typography';
-import { useEffect, useState } from 'react';
-import { default as fisrtData } from './data/kn_group1_result.json';
-import { default as secondData } from './data/kpzn_group1_result.json';
-import { appSt } from './style.css';
+import { ButtonMobile } from "@alfalab/core-components/button/mobile";
+import { Gap } from "@alfalab/core-components/gap";
+import {
+  SliderInput,
+  SliderInputProps,
+} from "@alfalab/core-components/slider-input";
+import { Typography } from "@alfalab/core-components/typography";
+import { PureCell } from "@alfalab/core-components/pure-cell";
+import { useEffect, useState } from "react";
+import { default as fisrtData } from "./data/kn_group1_result.json";
+import { default as secondData } from "./data/kpzn_group1_result.json";
+import { appSt } from "./style.css";
 
 const min = 30_000;
 const max = 1_600_000;
 const step = 10_000;
-const range: SliderInputProps['range'] = {
+const range: SliderInputProps["range"] = {
   min: [min],
   max: [max],
 };
-const pips: SliderInputProps['pips'] = {
-  mode: 'values',
+const pips: SliderInputProps["pips"] = {
+  mode: "values",
   values: [min, max],
   format: {
     to: (value: number) => {
-      return `${value.toLocaleString('ru')} ₽`;
+      return `${value.toLocaleString("ru")} ₽`;
     },
   },
 };
 
-function calculatePayment(principal: number, interestRate: number, term: number) {
-  const monthlyInterestRate = interestRate / 12;
-  const exponent = Math.pow(1 + monthlyInterestRate, term);
+// function calculatePayment(
+//   principal: number,
+//   interestRate: number,
+//   term: number
+// ) {
+//   const monthlyInterestRate = interestRate / 12;
+//   const exponent = Math.pow(1 + monthlyInterestRate, term);
 
-  return (principal * monthlyInterestRate * exponent) / (exponent - 1);
-}
+//   return (principal * monthlyInterestRate * exponent) / (exponent - 1);
+// }
 
 function findNearestValue(arr: number[], target: number) {
-  return arr.reduce((prev, curr) => (Math.abs(curr - target) < Math.abs(prev - target) ? curr : prev));
+  return arr.reduce((prev, curr) =>
+    Math.abs(curr - target) < Math.abs(prev - target) ? curr : prev
+  );
 }
 
 export const App = () => {
   const [value, setValue] = useState<number | string>(min);
 
-  const numberValue = typeof value === 'string' ? Number(value.replace(/ /g, '')) : value;
+  const numberValue =
+    typeof value === "string" ? Number(value.replace(/ /g, "")) : value;
   const dataset =
     numberValue <= 700_000
-      ? fisrtData[`${numberValue as unknown as keyof typeof fisrtData}`] ?? fisrtData[30000]
-      : secondData[`${numberValue as unknown as keyof typeof secondData}`] ?? secondData[700000];
+      ? fisrtData[`${numberValue as unknown as keyof typeof fisrtData}`] ??
+        fisrtData[30000]
+      : secondData[`${numberValue as unknown as keyof typeof secondData}`] ??
+        secondData[700000];
 
-  const pipsValuesMonthlyPayment = dataset.map(v => Number(v.value.toFixed(0))).sort((a, b) => a - b);
+  const pipsValuesMonthlyPayment = dataset
+    .map((v) => Number(v.value.toFixed(0)))
+    .sort((a, b) => a - b);
 
-  const [monthlyValue, setMonthlyValue] = useState<number | string>(pipsValuesMonthlyPayment[0]);
+  const [monthlyValue, setMonthlyValue] = useState<number | string>(
+    pipsValuesMonthlyPayment[0]
+  );
 
-  const monthlyNumberValue = typeof monthlyValue === 'string' ? Number(monthlyValue.replace(/ /g, '')) : monthlyValue;
+  const monthlyNumberValue =
+    typeof monthlyValue === "string"
+      ? Number(monthlyValue.replace(/ /g, ""))
+      : monthlyValue;
 
-  const pipsValuesPeriod = dataset.map(v => v.period).sort((a, b) => b - a);
+  const pipsValuesPeriod = dataset.map((v) => v.period).sort((a, b) => b - a);
   const [periodValue, setPeriodValue] = useState<number>(pipsValuesPeriod[0]);
-
-  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     setMonthlyValue(pipsValuesMonthlyPayment[0]);
@@ -72,11 +88,18 @@ export const App = () => {
     setMonthlyValue(pipsValuesMonthlyPayment[index === -1 ? 0 : index]);
   }, [periodValue]);
 
-  const handleInputChange: SliderInputProps['onInputChange'] = (_, { value }) => {
-    setValue(typeof value === 'string' ? Number(value.replace(/ /g, '')) : value);
+  const handleInputChange: SliderInputProps["onInputChange"] = (
+    _,
+    { value }
+  ) => {
+    setValue(
+      typeof value === "string" ? Number(value.replace(/ /g, "")) : value
+    );
   };
 
-  const handleSliderChange: SliderInputProps['onSliderChange'] = ({ value }) => {
+  const handleSliderChange: SliderInputProps["onSliderChange"] = ({
+    value,
+  }) => {
     setValue(value);
   };
 
@@ -84,11 +107,18 @@ export const App = () => {
     setValue(Math.max(min, Math.min(max, numberValue)));
   };
 
-  const handleMInputChange: SliderInputProps['onInputChange'] = (_, { value }) => {
-    setMonthlyValue(typeof value === 'string' ? Number(value.replace(/ /g, '')) : value);
+  const handleMInputChange: SliderInputProps["onInputChange"] = (
+    _,
+    { value }
+  ) => {
+    setMonthlyValue(
+      typeof value === "string" ? Number(value.replace(/ /g, "")) : value
+    );
   };
 
-  const handleMSliderChange: SliderInputProps['onSliderChange'] = ({ value }) => {
+  const handleMSliderChange: SliderInputProps["onSliderChange"] = ({
+    value,
+  }) => {
     setMonthlyValue(findNearestValue(pipsValuesMonthlyPayment, value));
   };
 
@@ -98,16 +128,24 @@ export const App = () => {
         pipsValuesMonthlyPayment,
         Math.max(
           pipsValuesMonthlyPayment[0],
-          Math.min(pipsValuesMonthlyPayment[pipsValuesMonthlyPayment.length - 1], monthlyNumberValue),
-        ),
-      ),
+          Math.min(
+            pipsValuesMonthlyPayment[pipsValuesMonthlyPayment.length - 1],
+            monthlyNumberValue
+          )
+        )
+      )
     );
   };
-  const handlePInputChange: SliderInputProps['onInputChange'] = (_, { value }) => {
-    setPeriodValue(value === '' ? pipsValuesPeriod[0] : value);
+  const handlePInputChange: SliderInputProps["onInputChange"] = (
+    _,
+    { value }
+  ) => {
+    setPeriodValue(value === "" ? pipsValuesPeriod[0] : value);
   };
 
-  const handlePSliderChange: SliderInputProps['onSliderChange'] = ({ value }) => {
+  const handlePSliderChange: SliderInputProps["onSliderChange"] = ({
+    value,
+  }) => {
     setPeriodValue(findNearestValue(pipsValuesPeriod, value));
   };
 
@@ -115,19 +153,23 @@ export const App = () => {
     setPeriodValue(
       findNearestValue(
         pipsValuesPeriod,
-        Math.max(pipsValuesPeriod[pipsValuesPeriod.length - 1], Math.min(pipsValuesPeriod[0], periodValue)),
-      ),
+        Math.max(
+          pipsValuesPeriod[pipsValuesPeriod.length - 1],
+          Math.min(pipsValuesPeriod[0], periodValue)
+        )
+      )
     );
-  };
-
-  const handleToggle = () => {
-    setExpanded(!expanded);
   };
 
   return (
     <>
       <div className={appSt.container}>
-        <Typography.TitleResponsive font="system" tag="h1" view="small" weight="medium">
+        <Typography.TitleResponsive
+          font="system"
+          tag="h1"
+          view="small"
+          weight="medium"
+        >
           Выберите свои условия кредита
         </Typography.TitleResponsive>
         <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
@@ -135,7 +177,7 @@ export const App = () => {
         </Typography.Text>
         <SliderInput
           block
-          value={value.toLocaleString('ru')}
+          value={value.toLocaleString("ru")}
           sliderValue={numberValue}
           onInputChange={handleInputChange}
           onSliderChange={handleSliderChange}
@@ -155,7 +197,7 @@ export const App = () => {
 
         <SliderInput
           block
-          value={monthlyValue.toLocaleString('ru')}
+          value={monthlyValue.toLocaleString("ru")}
           sliderValue={monthlyNumberValue}
           onInputChange={handleMInputChange}
           onSliderChange={handleMSliderChange}
@@ -164,14 +206,16 @@ export const App = () => {
           max={pipsValuesMonthlyPayment[pipsValuesMonthlyPayment.length - 1]}
           range={{
             min: [pipsValuesMonthlyPayment[0]],
-            max: [pipsValuesMonthlyPayment[pipsValuesMonthlyPayment.length - 1]],
+            max: [
+              pipsValuesMonthlyPayment[pipsValuesMonthlyPayment.length - 1],
+            ],
           }}
           pips={{
-            mode: 'range',
+            mode: "range",
             values: pipsValuesMonthlyPayment,
             format: {
               to: (v: number) => {
-                return `${v.toLocaleString('ru')} ₽`;
+                return `${v.toLocaleString("ru")} ₽`;
               },
             },
           }}
@@ -197,7 +241,7 @@ export const App = () => {
             min: [pipsValuesPeriod[pipsValuesPeriod.length - 1]],
           }}
           pips={{
-            mode: 'range',
+            mode: "range",
             values: pipsValuesPeriod,
             format: {
               to: (v: number) => {
@@ -213,33 +257,56 @@ export const App = () => {
           labelView="outer"
         />
 
-        <div>
-          <Typography.Text tag="p" view="primary-small" defaultMargins={false}>
+        <div className={appSt.moneyCard}>
+          <Typography.Text
+            tag="p"
+            view="primary-small"
+            defaultMargins={false}
+            style={{ color: "#3C3C43A8" }}
+          >
             Переплата за весь срок
           </Typography.Text>
-          <Typography.Text weight="bold" tag="p" view="primary-medium" defaultMargins={false}>
-            {(monthlyNumberValue * periodValue - numberValue).toLocaleString('ru')} ₽
+          <Typography.Text
+            weight="bold"
+            tag="p"
+            view="primary-medium"
+            defaultMargins={false}
+          >
+            {(monthlyNumberValue * periodValue - numberValue).toLocaleString(
+              "ru"
+            )}{" "}
+            ₽
           </Typography.Text>
         </div>
+        <Gap size={8} />
 
-        <div onClick={handleToggle} className={appSt.collapseAction}>
-          <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
-            Как еще увеличить лимит?
+        <Typography.TitleResponsive
+          font="system"
+          tag="h2"
+          view="xsmall"
+          weight="medium"
+        >
+          Выберите предложение
+        </Typography.TitleResponsive>
+
+        <div className={appSt.card}>
+          <Typography.Text
+            tag="p"
+            view="component"
+            defaultMargins={false}
+            weight="bold"
+          >
+            43 000 ₽ / мес
           </Typography.Text>
-          <CDNIcon name="glyph_chevron-down_m" className={appSt.collapseArrow({ open: expanded })} />
+          <Typography.Text
+            tag="p"
+            view="component-secondary"
+            defaultMargins={false}
+            color="secondary"
+          >
+            На приобретение автомобиля под залог старого
+          </Typography.Text>
         </div>
-
-        <Collapse expanded={expanded}>
-          <Typography.Text view="primary-small" tag="p" style={{ color: '#7F7F83' }}>
-            Предоставьте справку о доходах или станьте зарплатным клиентом Альфа-Банка
-          </Typography.Text>
-          <Typography.Text view="primary-small" tag="p" style={{ color: '#7F7F83' }}>
-            Тратьте от 100 000 ₽ в месяц с нашей кредитной картой
-          </Typography.Text>
-          <Typography.Text view="primary-small" tag="p" style={{ color: '#7F7F83' }}>
-            Оформите подписку на Альфа-Жизнь
-          </Typography.Text>
-        </Collapse>
       </div>
       <Gap size={96} />
       <div className={appSt.bottomBtn}>
